93f23891800d016bfbadee09e30fde0aefd47023
Kagami <miffysaya@sina.cn>
2017-05-11 13:01:37 +0800

    20170511 13:01

---
+++ AgileFramework/Business/AgilePDASoap.cs
@@ -0,0 +1,117 @@
+﻿using AgileFramework.Text;
+using AgileFramework.Web;
+using Newtonsoft.Json;
+using System.Xml;
+
+namespace AgileFramework.Business
+{
+    /// <summary>
+    /// 对PDA接口进行操作
+    /// </summary>
+    public static class AgilePDASoap
+    {
+        /// <summary>
+        /// 通过GET方式访问接口
+        /// </summary>
+        /// <param name="address">接口地址</param>
+        /// <param name="methodName">接口方法</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public static AgilePDAEntity Get(string address, AgilePDAMethod methodName, string parameters = "")
+        {
+            var url = address.TrimEnd('/') + "/" + methodName.ToString() + (string.IsNullOrWhiteSpace(parameters) ? "" : "?" + parameters);
+
+            var html = AgileRequest.GetHtml(url);
+
+            return ParseEntity(html);
+        }
+
+        /// <summary>
+        /// 通过POST方式访问接口
+        /// </summary>
+        /// <param name="address">接口地址</param>
+        /// <param name="methodName">接口方法</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public static AgilePDAEntity Post(string address, AgilePDAMethod methodName, string parameters = "")
+        {
+            var url = address.TrimEnd('/') + "/" + methodName.ToString();
+
+            var html = AgileRequest.GetHtml(url, AgileEncoding.Default, 1000, AgileHttpActionType.Post, parameters);
+
+            return ParseEntity(html);
+        }
+
+        /// <summary>
+        /// 将XML解析为实体类的方法
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        private static AgilePDAEntity ParseEntity(string html)
+        {
+            if (string.IsNullOrEmpty(html))
+            {
+                return null;
+            }
+            var document = new XmlDocument();
+
+            document.LoadXml(html);
+
+            var jsonText = JsonConvert.SerializeXmlNode(document);
+
+            var dynamicObject = JsonConvert.DeserializeObject<dynamic>(jsonText);
+
+            if (dynamicObject.Feedback == null)
+            {
+                return null;
+            }
+            return new AgilePDAEntity()
+            {
+                Status = dynamicObject.Feedback.Status,
+                Error = dynamicObject.Feedback.Error,
+                Result = dynamicObject.Feedback.Result
+            };
+        }
+    }
+
+    /// <summary>
+    /// PDA接口返回结果实体类
+    /// </summary>
+    public class AgilePDAEntity
+    {
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// 结果
+        /// </summary>
+        public string Result { get; set; }
+    }
+
+    /// <summary>
+    /// PDA接口方法枚举
+    /// </summary>
+    public enum AgilePDAMethod
+    {
+        GetList,
+
+        GetOrderStatus,
+
+        GrabList,
+
+        GrabToBind,
+
+        Login,
+
+        LoginAMS,
+
+        UpdateDT
+    }
+}
---
+++ AgileFramework/Configuration/AgileConfig.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AgileFramework.Configuration
+{
+    public static class AgileConfig
+    {
+        /// <summary>
+        /// 解析为数组
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="value">值</param>
+        /// <param name="split">分割符</param>
+        /// <param name="isDecode">是否解码</param>
+        /// <returns>结果</returns>
+        public static T[] ParseArray<T>(string value, char split = '|', bool isDecode = true)
+        {
+            var type = typeof(T);
+            if (type == typeof(string) && isDecode)
+            {
+                value = HttpUtility.HtmlDecode(value);
+            }
+            T[] array = null;
+            if (!string.IsNullOrEmpty(value))
+            {
+                array = value.Split(split).Select(item =>
+                {
+                    if (type.IsEnum)
+                    {
+                        return (T)Enum.Parse(type, item, true);
+                    }
+                    else
+                    {
+                        return (T)Convert.ChangeType(item, typeof(T));
+                    }
+                }).ToArray();
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// 获得值
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="key">键</param>
+        /// <param name="isDecode">是否解码</param>
+        /// <returns>结果</returns>
+        public static T GetValue<T>(string key, bool isDecode = true)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            var type = typeof(T);
+            if (type == typeof(string) && isDecode)
+            {
+                value = HttpUtility.HtmlDecode(value);
+            }
+            if (type.IsEnum)
+            {
+                return (T)Enum.Parse(type, value, true);
+            }
+            else
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+        }
+
+        /// <summary>
+        /// 获得值
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="key">键</param>
+        /// <param name="split">分割符</param>
+        /// <param name="isDecode">是否解码</param>
+        /// <returns>结果</returns>
+        public static T[] GetValues<T>(string key, char split = '|', bool isDecode = true)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            return ParseArray<T>(value, split, isDecode);
+        }
+
+        /// <summary>
+        /// 刷新配置
+        /// </summary>
+        public static void Refresh()
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
